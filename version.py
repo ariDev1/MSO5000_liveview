d4e9571a3aeb5e0443eabe41feaf7b6dda9b35fb
ariDev1 <mail@aether-research.institute>
2025-07-19 19:44:00 +0200

    update screenshot.png to v0.9.4

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 APP_NAME = "MSO5000 Live Monitor"
 VERSION = "v0.9.4"
-GIT_COMMIT = "31bd845"
-BUILD_DATE = "2025-07-19 17:38 UTC"
+GIT_COMMIT = "ad19631"
+BUILD_DATE = "2025-07-19 17:41 UTC"
 AUTHOR = "ariDev1"
 PROJECT_URL = "https://github.com/ariDev1/MSO5000_liveview"
