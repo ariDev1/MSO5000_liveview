ee162c55aeaa55962938b70e42312abf227f9619
ariDev1 <mail@aether-research.institute>
2025-07-30 10:47:32 +0200

    checktestfile1

--- scpi/checkVISAbackend.py
+++ scpi/checkVISAbackend.py
@@ -1,4 +1,15 @@
 import pyvisa
-print(pyvisa.__version__)
-rm = pyvisa.ResourceManager()
-print(rm)
+
+rm = pyvisa.ResourceManager('@py')
+resources = rm.list_resources()
+print("Resources:", resources)
+
+for res in resources:
+    if "USB" in res:
+        try:
+            print(f"🔌 Trying {res} ...")
+            instr = rm.open_resource(res)
+            idn = instr.query("*IDN?")
+            print(f"✅ IDN: {idn}")
+        except Exception as e:
+            print(f"❌ Failed to connect to {res}: {e}")
