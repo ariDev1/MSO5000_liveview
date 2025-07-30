138ea95bf14981f77e5b43f9ec1428f7b5f0d3d2
ariDev1 <mail@aether-research.institute>
2025-07-30 20:58:56 +0200

    optimize pq-plot

--- version.py
+++ version.py
@@ -1,6 +1,12 @@
 APP_NAME = "MSO5000 Live Monitor"
+<<<<<<< HEAD
 VERSION = "v0.9.8b"
 GIT_COMMIT = "d11e3fa"
 BUILD_DATE = "2025-07-31 15:27 UTC"
+=======
+VERSION = "v0.9.8"
+GIT_COMMIT = "6400c23"
+BUILD_DATE = "2025-07-30 18:54 UTC"
+>>>>>>> 5f0cf42 (optimize pq-plot)
 AUTHOR = "ariDev1"
 PROJECT_URL = "https://github.com/ariDev1/MSO5000_liveview"
