660cbddc54124a05dd405cdf61b5bef483f4e136
ariDev1 <mail@aether-research.institute>
2025-07-31 19:35:53 +0200

    Restore latest updates: GUI, SCPI, version, whitepaper

--- version.py
+++ version.py
@@ -1,12 +1,6 @@
 APP_NAME = "MSO5000 Live Monitor"
-<<<<<<< HEAD
 VERSION = "v0.9.8b"
-GIT_COMMIT = "d11e3fa"
-BUILD_DATE = "2025-07-31 15:27 UTC"
-=======
-VERSION = "v0.9.8"
-GIT_COMMIT = "6400c23"
-BUILD_DATE = "2025-07-30 18:54 UTC"
->>>>>>> 5f0cf42 (optimize pq-plot)
+GIT_COMMIT = "c588f24"
+BUILD_DATE = "2025-07-31 17:00 UTC"
 AUTHOR = "ariDev1"
 PROJECT_URL = "https://github.com/ariDev1/MSO5000_liveview"
