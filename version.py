0ab61283c21becccd4232339d6ce0b5037c0931d
ariDev1 <mail@aether-research.institute>
2025-08-20 23:20:33 +0200

    final polish..

---
+++ version.py
@@ -0,0 +1,6 @@
+APP_NAME = "MSO5000 Live Monitor"
+VERSION = "v0.9.8h-stable"
+GIT_COMMIT = "9637d3f"
+BUILD_DATE = "2025-08-20 21:19 UTC"
+AUTHOR = "ariDev1"
+PROJECT_URL = "https://github.com/ariDev1/MSO5000_liveview"
